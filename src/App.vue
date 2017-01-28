2866e6d288da8132441bdfe68fab71a39c30bfb9
vxtals <vxtals@gmail.com>
2017-01-27 22:54:15 -0600

    Styles moved to global app styles file. Removed hello component. Added tripmap component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <tripmap></tripmap>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello'
+import Tripmap from './components/tripmap/Tripmap'
 
 export default {
   name: 'app',
   components: {
-    Hello
+    Tripmap
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src='./main.scss'></style>
