2ce363b12345c91ac1ce66eb4d63a4fff483eefe
Victor Portals Lorenzo <Victor Portals Lorenzo>
2017-02-14 23:40:55 -0600

    Modified library loading

---
+++ src/components/App.vue
@@ -0,0 +1,7 @@
+<template lang='jade' src="./app.view.jade"></template>
+<script src="./app.controller.js"/></script>
+<style lang='scss' src='./app.style.scss'></style>
+
+<style src='../assets/styles/bootstrap.css'></style>
+<style src='../assets/styles/material.blue-orange.min.css'></style>
+<style src='../assets/styles/materialFontIcons.css'></style>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './components/App'
 import VueMdl from 'vue-mdl'
-
 import Sortable from 'sortablejs'
+
+window._ = require('lodash')
+window.$ = require('jquery')
+
+require('material-design-lite')
 
 /* eslint-disable no-new */
 Vue.directive('sortable', {
@@ -13,14 +17,15 @@ Vue.directive('sortable', {
   }
 })
 
-/* eslint-disable no-new */
-window.eventBus = new Vue()
-
 Vue.use(VueMdl)
+
+/* eslint-disable no-new */
+// New vue instance to be used as event bus
+// It must be before Vue app instantation
+window.eventBus = new Vue()
 
 new Vue({
   el: '#app',
   template: '<App/>',
   components: { App }
 })
-
