a553bc880b75beb8ea3499e9a7c0a1c821ffa060
vxtals <vxtals@gmail.com>
2017-02-24 10:53:20 -0600

    Added new triplist route entry

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 import NewStage from './components/newStage/NewStage'
+import TripList from './components/tripList/TripList'
 
 const routes = [
-  {path: '/new-stage', component: NewStage},
-  {path: '/', component: NewStage}
+  {
+    path: '/',
+    component: TripList
+  },
+  {
+    path: '/newstage',
+    component: NewStage
+  },
+  {
+    path: '/triplist',
+    component: TripList
+  }
 ]
 
 export default routes
