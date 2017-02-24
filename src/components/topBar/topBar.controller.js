a56f17bb0478adaa319073c15f2a9c27077d7827
vxtals <vxtals@gmail.com>
2017-02-24 10:54:07 -0600

    Improved side drawer behaviour

--- src/components/topBar/topBar.controller.js
+++ src/components/topBar/topBar.controller.js
@@ -7,6 +7,17 @@ export default {
     return {
       
     }
+  },
+  directives: {
+    autofold: (el) => {
+      let links = el.getElementsByTagName('a')
+      for (var i = 0; i < links.length; i++){
+        links[i].addEventListener('click', ()=>{
+          el.classList.toggle('is-visible')
+          document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.toggle('is-visible')
+        })
+      }
+    }
   }
 }
 
