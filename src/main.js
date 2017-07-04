fdb104d89c41b5a52067afe2b1a0106341c923e8
vxtals <vxtals@gmail.com>
2017-07-04 12:11:36 -0500

    mdl require removed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './components/App'
@@ -9,8 +10,6 @@ import Sortable from 'sortablejs'
 
 window._ = require('lodash')
 window.$ = require('jquery')
-
-require('material-design-lite')
 
 /* eslint-disable no-new */
 Vue.directive('sortable', {
